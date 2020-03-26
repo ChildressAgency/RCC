b76509d170242506e8af0a77d2b38f0a9f64d0c3
Marina Nicolai <nicolaimarina@gmail.com>
2020-03-26 06:19:56 -0400

    initial commit

---
+++ wp-theme-files/footer.php
@@ -0,0 +1,46 @@
+<!--FOOTER
+       ==================================================================================-->  
+       <footer>
+          <div class="container">
+            <div class="row">
+               <div class="col-6">
+                 <h2>Contact us</h2>
+                  <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                       <i class=""></i><h3>11120 Gordon Road, Fredericksburg, VA 22407</h3>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <i class=""></i><h3>Office Hours:<br>
+                                          Monday - Friday: 8:30 am - 5:00 pm<br>
+                                          Saturday - Hours Vary (by appointment only)<br>
+                                          Sunday: Closed</h3>
+                   </li>
+                   <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <i class=""></i><h3>Call:540.273.6685</h3>
+                 </li>
+               </div>
+               <div class="col-6">
+                 <a class="navbar-brand" href="#"><img src="" /></a>
+                 <ul class="list-group">
+                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                       <h3>Gaye B. Martson, LPC, LMFT</h3>
+                     </li> 
+                     <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <h3>Teresa Robisnon, M.A. Professional Counselor</h3>
+                    </li>
+                  </ul>
+                  <h3>Follow us</h3> 
+                  <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <i class="">FB</i> 
+                    </li> 
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <i class="">INSTA</i> 
+                    </li> 
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <i class="">TWITTER</i> 
+                    </li>  
+              </div>
+            </div>
+          </div>
+       </footer> 
---
+++ wp-theme-files/header.php
@@ -0,0 +1,22 @@
+<!--HEADER
+       ==================================================================================-->
+       <header class="site-hearder" role="banner">
+         <!--NAVBAR
+       ==================================================================================-->
+        <div class="navbar-wrapper">
+         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+           <ul> 
+            <li><a class="navbar-brand" href="#">Bussiness Hours</a></li>
+            <li><a class="navbar-brand" href="#">Call:540.273.6685</a></li>
+            <li><a class="navbar-brand" href="#">Schedule appointment</a></li>
+           </ul>
+            <ul class="navbar-text">
+               <li><a class="navbar-brand" href="#">FB</a></li>
+               <li><a class="navbar-brand" href="#">Instagram</a></li>
+               <li><a class="navbar-brand" href="#">Twitter</a></li>
+            </ul>
+            
+            </button>            
+          </nav>
+         </div><!--navbar-wrapper-->
+     </header>
