843c71437731f706582a232d32324e509eebd7af
Marina Nicolai <nicolaimarina@gmail.com>
2020-03-30 07:55:31 -0400

    pages

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Twenty Twenty functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+/**
+ * Register and Enqueue Styles.
+ */
+function twentytwenty_register_styles() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'twentytwenty-style', get_stylesheet_uri(), array(), $theme_version );
+	wp_style_add_data( 'twentytwenty-style', 'rtl', 'replace' );
+}
+
+add_action( 'wp_enqueue_scripts', 'twentytwenty_register_styles' );
---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The header for RCC theme
+ *
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="page" class="site">
+    <header class="" role="banner">         
+      <div class="site-top-header">                    
+            <a class="navbar-brand" href="#"><i class="far fa-clock"></i>Bussiness Hours</a>
+            <a class="navbar-brand" href="#"><i class="fas fa-phone-alt"></i>Call:540.273.6685</a>
+            <div class="right-side">
+              <a class="appointment" href="#">Schedule appointment</a>
+              <ul class="social-icons">
+                <li><a class="navbar-brand" href="#">FB</a></li>
+                <li><a class="navbar-brand" href="#">Instagram</a></li>
+                <li><a class="navbar-brand" href="#">Twitter</a></li>
+              </ul>            
+            </div>
+            
+            </button>            
+         </div><!--navbar-wrapper-->
+     </header>
